488ca370e1b920ada90220079b494c3dba88555e
Jai <jpate661@my.centennialcollege.ca>
2022-07-23 23:22:39 -0400

    Created Environment

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+const Page = () => {
+    return (
+        <div>hELLO</div>
+    )
+}
+
+export default Page
---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+const options = {
+    
+    arrowParens: 'avoid',
+    singlequote: true,
+    bracketSpacing: true,
+    endOfLine: 'lf',
+    semi: false,
+    tabwidth: 2,
+    trailing:'none'
+}
+
+module.exports =options
