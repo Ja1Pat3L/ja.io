a35dca9ad27c6995e312bab66ee1f37490c34c8c
Jai <jpate661@my.centennialcollege.ca>
2022-07-30 22:24:58 -0400

    added app.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,12 @@
+import { Container,Box } from "@chakra-ui/react"
 const Page = () => {
     return (
-        <div>hELLO</div>
+      <container>
+        <Box borderRadius="lg">
+          Hello I am a full stack developer. My name is Jai Patel
+        </Box>
+            
+      </container>
     )
 }
 
